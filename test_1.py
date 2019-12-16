5432476d704132de133a8a3448f7faf000b6166c
unknown <D:\The Bat!>
2019-12-16 10:46:34 +0200

    Add answer

--- test_1.py
+++ test_1.py
@@ -5,5 +5,3 @@ Hint: try to change the URL address.
 
 ans = 2 ** 38
 print('http://www.pythonchallenge.com/pc/def/', ans, '.html', sep='')
-
-#http://www.pythonchallenge.com/pc/def/274877906944.html
