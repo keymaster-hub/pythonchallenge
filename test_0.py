b4ea39fd1c2ce6559e34563d7f917c10a699fefd
unknown <D:\The Bat!>
2019-12-16 12:39:47 +0200

    Add test#0

--- test_0.py
+++ test_0.py
@@ -1,8 +1,8 @@
 """
 http://www.pythonchallenge.com/pc/def/0.html
 Hint: try to change the URL address.
-"""
 
+"""
 ans = 2 ** 38
 print('http://www.pythonchallenge.com/pc/def/', ans, '.html', sep='')
 
