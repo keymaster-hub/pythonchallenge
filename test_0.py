a0f19cbb5cc3197675c3e0043bf677fb71803d95
unknown <D:\The Bat!>
2019-12-16 10:44:33 +0200

    Add text

--- test_0.py
+++ test_0.py
@@ -1,3 +1,7 @@
-#Hint: try to change the URL address.
+"""
+http://www.pythonchallenge.com/pc/def/0.html
+Hint: try to change the URL address.
+"""
+
 ans = 2 ** 38
 print('http://www.pythonchallenge.com/pc/def/', ans, '.html', sep='')
