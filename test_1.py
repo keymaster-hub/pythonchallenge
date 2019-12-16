27a8502e14f1880c61ad6881093ca2556c6b3547
unknown <D:\The Bat!>
2019-12-16 10:56:11 +0200

    add test#1

--- test_1.py
+++ test_1.py
@@ -1,7 +1,28 @@
 """
-http://www.pythonchallenge.com/pc/def/0.html
-Hint: try to change the URL address.
+http://www.pythonchallenge.com/pc/def/map.html
 """
 
-ans = 2 ** 38
-print('http://www.pythonchallenge.com/pc/def/', ans, '.html', sep='')
+import string
+
+
+def decrypt(word: string, step: int):
+    step = step % 26
+    alph = string.ascii_lowercase + string.ascii_lowercase[0:step] +\
+           string.ascii_uppercase + string.ascii_uppercase[0:step]
+    list = []
+    for x in word:
+        if x.isalpha():
+            list += [alph[alph.index(x) + step]]
+        else:
+            list += x
+    return list
+
+
+question = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr\
+amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr \
+ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle \
+qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+print(*decrypt(question, 2), sep='')
+print('http://www.pythonchallenge.com/pc/def/', \
+      (*decrypt('map', 2)), '.html', sep='')
+#http://www.pythonchallenge.com/pc/def/ocr.html
