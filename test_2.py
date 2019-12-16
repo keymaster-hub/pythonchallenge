f426bd93b06d1802c60c19daef33258821a1be06
unknown <D:\The Bat!>
2019-12-16 12:37:23 +0200

    Add test#2

---
+++ test_2.py
@@ -0,0 +1,29 @@
+"""
+http://www.pythonchallenge.com/pc/def/ocr.html
+<!--
+find rare characters in the mess below:
+-->
+"""
+
+import urllib.request
+data = str(urllib.request.urlopen\
+           ("http://www.pythonchallenge.com/pc/def/ocr.html").read())
+
+data = ((max(data.split(), key=len)))[19:-10:]
+
+dct = {}
+for i in data:
+    if i in dct:
+        dct[i] += 1
+    else:
+        dct[i] = 1
+def rare_word():
+    rare = ''
+    for key, value in dct.items():
+        if value == 1:
+            rare += str(key)
+    return rare            
+
+print('http://www.pythonchallenge.com/pc/def/', rare_word(), '.html', sep='')
+
+#http://www.pythonchallenge.com/pc/def/equality.html
