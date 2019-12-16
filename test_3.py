528db0e516c83639d0aae0484ef8f3d634c4c381
unknown <D:\The Bat!>
2019-12-16 15:42:27 +0200

    Working but wrong :D

--- test_3.py
+++ test_3.py
@@ -1,7 +1,8 @@
 """
 http://www.pythonchallenge.com/pc/def/ocr.html
 <!--
-find rare characters in the mess below:
+One small letter, surrounded by EXACTLY three big bodyguards
+on each of its sides.
 -->
 """
 
@@ -10,16 +11,40 @@ data = str(urllib.request.urlopen\
            ("http://www.pythonchallenge.com/pc/def/equality.html").read())
 
 data = ((max(data.split(), key=len)))
-data = ((max(data.split('--'), key=len)))
-dct = {}
-for i in data:
-    if i in dct:
-        dct[i] += 1
-    else:
-        dct[i] = 1
+data = 'xxxx' + ((max(data.split('--'), key=len))) + 'xxxx'
 
-for key, value in dct.items():
-    print(key, value)
+
+def secret_word():
+    word = ''
+    full = '' 
+    for i in range(len(data)):
+        if data[i].islower():
+            
+            word += data[i]
+        elif data[i].isupper():
+            if data[i:i+3].isupper() and \
+               data[i-3-len(word):i-len(word)].isupper() and\
+               data[i+3].islower() and \
+               data[i-4-len(word)].islower():
+                    
+               
+                    full +=  word
+                    
+                
+            else:
+                word = ''
+    return(full)            
+print(secret_word()) 
+
+                                              
+                
+                    
+
+         
+           
+            
+                
+print(secret_word())
 
     
 
