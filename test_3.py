cc80976fa451ef8b8a5da7d1defac37caffd8793
Keymaster-hub <keymasterwow@gmail.com>
2019-12-17 00:12:42 +0200

    Add test#3

--- test_3.py
+++ test_3.py
@@ -1,5 +1,5 @@
 """
-http://www.pythonchallenge.com/pc/def/ocr.html
+http://www.pythonchallenge.com/pc/def/equality.html
 <!--
 One small letter, surrounded by EXACTLY three big bodyguards
 on each of its sides.
@@ -10,44 +10,21 @@ import urllib.request
 data = str(urllib.request.urlopen\
            ("http://www.pythonchallenge.com/pc/def/equality.html").read())
 
-data = ((max(data.split(), key=len)))
-data = 'xxxx' + ((max(data.split('--'), key=len))) + 'xxxx'
-
+data = (max(data.split(), key=len))
+data = (max(data.split('--'), key=len))
+data = data.replace('\\n','')
 
 def secret_word():
     word = ''
-    full = '' 
     for i in range(len(data)):
-        if data[i].islower():
-            
+        if data[i].islower() and \
+           data[i-3:i].isupper() and \
+           data[i+1:i+4].isupper() and \
+           data[i-4].islower() and \
+           data[i+4].islower():
             word += data[i]
-        elif data[i].isupper():
-            if data[i:i+3].isupper() and \
-               data[i-3-len(word):i-len(word)].isupper() and\
-               data[i+3].islower() and \
-               data[i-4-len(word)].islower():
-                    
-               
-                    full +=  word
-                    
-                
-            else:
-                word = ''
-    return(full)            
-print(secret_word()) 
+    return word
 
-                                              
-                
-                    
+print('http://www.pythonchallenge.com/pc/def/', secret_word(), '.php', sep='')
 
-         
-           
-            
-                
-print(secret_word())
-
-    
-
-#print('http://www.pythonchallenge.com/pc/def/', rare_word(), '.html', sep='')
-
-#http://www.pythonchallenge.com/pc/def/equality.html
+#http://www.pythonchallenge.com/pc/def/linkedlist.php
