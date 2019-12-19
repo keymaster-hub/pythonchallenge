3df8a259255a69c959f89700879260c9d4f3fe96
unknown <D:\The Bat!>
2019-12-19 10:02:48 +0200

    Add test#5

--- test_5.py
+++ test_5.py
@@ -3,22 +3,19 @@ http://www.pythonchallenge.com/pc/def/peak.html
 http://www.pythonchallenge.com/pc/def/banner.p
 """
 
-import urllib.request
+from urllib.request import urlopen
 import pickle
 
 base_url = 'http://www.pythonchallenge.com/pc/def/banner.p'
-data = urllib.request.urlopen(base_url).read()
+data = urlopen(base_url).read()
+
 data = pickle.loads(data)
-for i in range(len(data)):
-    print(data[i])
-    
-
-
-"""
-data[0] = str(data[0][2:])
-data[-1] = str(data[-1][0:-1])
 for i in data:
-    print(i)
-"""
+    print("".join([a * b for a, b in i]))
+    
+print('http://www.pythonchallenge.com/pc/def/channel.html')
+#http://www.pythonchallenge.com/pc/def/channel.html
+
+
 
 
